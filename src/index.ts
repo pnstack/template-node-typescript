c8010905ac38f3dc10b6111b8f4e6797ba4d5077
Phạm Văn Nguyên <npv2k1@gmail.com>
2023-08-04 00:43:02 +0700

    update

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const hello = (name: string) => {
+  return `Hello ${name}`;
+};
+
+async function bootstrap() {
+  console.log('App started jhsbcdh okockdsock', hello('world scd cds sdc'));
+}
+bootstrap();
