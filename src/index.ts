7404e261525884c151533ec1daea9ccced5b0ccc
Phạm Văn Nguyên <npv2k1@gmail.com>
2023-09-04 17:40:08 +0700

    update: update packages.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-const hello = (name: string) => {
-  return `Hello ${name}`;
-};
-
 async function bootstrap() {
-  console.log('App started jhsbcdh okockdsock', hello('world scd cds sdc'));
+  console.log('Hello');
 }
 bootstrap();
